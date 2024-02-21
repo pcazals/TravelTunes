4bd4e07b5478a0ec201b928f8d08c74c6a3c98ff
pcazals <82263236+pcazals@users.noreply.github.com>
2024-02-21 11:31:27 +0100

    V1

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+import WazeRouteCalculator
 
 app = Flask(__name__)
+Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Web App with Python Flask!'
+    travel_time = None
+    distance = None
+    if request.method == 'POST':
+        start_address = request.form.get('start_address')
+        end_address = request.form.get('end_address')
+        region = request.form.get('region', 'EU')
+        try:
+            route = WazeRouteCalculator.WazeRouteCalculator(start_address, end_address, region)
+            route_info = route.calc_route_info()
+            travel_time = route_info[0]
+            distance = route_info[1]
+        except WazeRouteCalculator.WRCError as e:
+            travel_time = e
+    return render_template('index.html', travel_time=travel_time, distance=distance)
 
-app.run(host='0.0.0.0', port=80)
+if __name__ == '__main__':
+    app.run(debug=True)
