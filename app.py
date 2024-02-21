968a0562f2ebfe033ba06eea1fe7421ba0507def
pcazals <82263236+pcazals@users.noreply.github.com>
2024-02-21 14:37:38 +0100

    CalculOK

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 from flask import Flask, request, render_template
 import WazeRouteCalculator
+import logging
 
 app = Flask(__name__)
+
+# Configuration du logging
+logger = logging.getLogger('WazeRouteCalculator.WazeRouteCalculator')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        start_address = request.form['start_address']
-        end_address = request.form['end_address']
-        region = 'EU' 
+        start_lat = request.form['start_lat']
+        start_lng = request.form['start_lng']
+        end_lat = request.form['end_lat']
+        end_lng = request.form['end_lng']
+        region = 'EU'
         try:
-            route = WazeRouteCalculator.WazeRouteCalculator(start_address, end_address, region)
+            route = WazeRouteCalculator.WazeRouteCalculator(f"{start_lat},{start_lng}", f"{end_lat},{end_lng}", region)
             route_info = route.calc_route_info()
             result = f"Durée estimée: {route_info[0]} minutes, Distance: {route_info[1]} km"
+            print(result)        
         except Exception as e:
             result = f"Erreur: {str(e)}"
-        
+            print(result)
         return render_template('index.html', result=result)
     else:
         return render_template('index.html', result=None)
